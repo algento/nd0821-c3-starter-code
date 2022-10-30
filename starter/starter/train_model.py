58ff0bf356f2847ba2e296aa05f413a7d410ee13
Sejong Heo <tromberx@gmail.com>
2022-10-30 13:29:49 +0900

    update: slicing_metric and model_card

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -11,6 +11,7 @@ import sys
 import pandas as pd
 
 import logging
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
@@ -19,8 +20,8 @@ sys.path.insert(0, file_dir)
 
 
 # Add code to load in the data.
-logger.info('Read clean_census.csv')
-data = pd.read_csv(file_dir + '/../data/clean_census.csv')
+logger.info("Read clean_census.csv")
+data = pd.read_csv(file_dir + "/../data/clean_census.csv")
 # data = pd.read_csv(file_dir + '/../data/census.csv', sep=', ', engine='python')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
@@ -46,7 +47,12 @@ X_train, y_train, encoder, lb = process_data(
 # Proces the test data with the process_data function.
 logger.info("Processing test data")
 X_test, y_test, encoder, lb = process_data(
-    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb,
 )
 
 # Train and save a model
@@ -55,10 +61,9 @@ rf_model = train_model(X_train, y_train)
 
 y_pred = inference(rf_model, X_test)
 precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
-logger.info(
-    f"Precision: {precision: .1f}, Recall: {recall: .1f}, Fbeta: {fbeta: .1f}")
+logger.info(f"Precision: {precision: .1f}, Recall: {recall: .1f}, Fbeta: {fbeta: .1f}")
 
 logger.info("Saving model")
-dump(rf_model, file_dir + '/../model/census_rfmodel.pkl')
-dump(encoder, file_dir + '/../model/census_encoder.pkl')
-dump(lb, file_dir + '/../model/census_lb.pkl')
+dump(rf_model, file_dir + "/../model/census_rfmodel.pkl")
+dump(encoder, file_dir + "/../model/census_encoder.pkl")
+dump(lb, file_dir + "/../model/census_lb.pkl")
